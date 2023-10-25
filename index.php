fe0d58bbf19874d873cbf3e8b6f359ae11c12795
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-10-25 21:00:54 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php include("lib/layouts/header.php"); ?>
 <link rel="stylesheet" href="css/style.css">
 
-
+<div class="home-main">
+    <div class="container">
+        <h2>Addict to Learning</h2>
+    </div>
+</div>
 
 <script src="js/script.js"></script>
