d0bcf02c34f5886a32d3567281fec358578da800
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-10-26 19:29:50 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
 
                     <p class="info-red"><i class="fas fa-exclamation-circle"></i> quisquam necessitatibus quibusdam consectetur, debitis.</p>
 
-                    <a href=""><button class="btn btn-primary">Start As a School</button></a>
+                    <a href=""><button class="btn btn-primary">Start As a Teacher</button></a>
                 </div>
             </div>
         </div>
@@ -67,7 +67,7 @@
 
                     <p class="info-red"><i class="fas fa-exclamation-circle"></i> quisquam necessitatibus quibusdam consectetur, debitis.</p>
 
-                    <a href=""><button class="btn btn-primary">Start As a School</button></a>
+                    <a href=""><button class="btn btn-primary">Start As a Student</button></a>
                 </div>
             </div>
             <div class="col-lg-4"></div>
