e4e30e7cdebadae21b9ae87c1fddbed12032bcab
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-10-26 19:32:33 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
                     <p class="info-green"><i class="far fa-check-circle"></i> debitis doloremque ad facilis corporis.</p>
                     <p class="info-green"><i class="far fa-check-circle"></i> Aliquid incidunt nam tenetur quisquam.</p>
                     <p class="info-green"><i class="far fa-check-circle"></i> quisquam necessitatibus quibusdam consectetur, debitis.</p>
-                    
+
 
                     <p class="info-yellow"><i class="fas fa-exclamation-triangle"></i> Aliquid incidunt nam tenetur quisquam.</p>
                     <p class="info-yellow"><i class="fas fa-exclamation-triangle"></i> quisquam necessitatibus quibusdam consectetur, debitis.</p>
@@ -33,7 +33,7 @@
             </div>
             <div class="col-lg-2">&nbsp;&nbsp;</div>
             <div class="col-lg-5 plan-info">
-            <div class="plan-head">Teacher</div>
+                <div class="plan-head">Teacher</div>
                 <div class="plan-body">
                     <p class="info-green"><i class="far fa-check-circle"></i> Lorem ipsum dolor sit amet.</p>
                     <p class="info-green"><i class="far fa-check-circle"></i> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
@@ -73,6 +73,18 @@
             <div class="col-lg-4"></div>
         </div>
 
+    </div>
+</section>
+
+<section class="start-school">
+    <div class="row">
+        <div class="col-lg-6">
+            <h1>Start As School</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat consequatur, odit id dolore non minima, aliquam, autem consectetur sequi alias omnis magni eligendi blanditiis quod error praesentium! Doloremque ratione suscipit libero voluptas laudantium quia laborum, et eaque iusto aliquam veniam quibusdam eos, fugit esse? Reiciendis ipsam aspernatur veniam deleniti cumque architecto corrupti nesciunt ducimus quas odio minus repudiandae blanditiis adipisci unde vitae a at suscipit itaque ex, consectetur est dicta hic beatae! Voluptate explicabo repudiandae quasi tempore dicta sit provident quos aperiam sapiente laudantium, excepturi quidem neque nobis ut enim iure quisquam mollitia facilis. Delectus distinctio tenetur illo amet dolor!
+            </p>
+        </div>
+        <div class="col-lg-6"></div>
     </div>
 </section>
 
